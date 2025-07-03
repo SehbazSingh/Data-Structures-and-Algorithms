69081092b32bb70e7343a0552b8ab3e366e98196
Sehbaz Singh <sehbazsingh58@gmail.com>
2025-07-03 18:03:46 +0530

    Update main.cpp

--- Maximum Subarray/main.cpp
+++ Maximum Subarray/main.cpp
@@ -22,7 +22,7 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int size = nums.size();
-        int maxsum = 0;
+        int maxsum = INT_MIN;
         for (int i = 0; i < size; ++i) {
             for (int j = i; j < size; ++j) {
                 int sum = 0;
