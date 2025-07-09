6245b04766ea35ef22bf26936ece2e32679e064f
Sehbaz Singh <sehbazsingh58@gmail.com>
2025-07-09 11:01:27 +0530

    Create ans.py

---
+++ Peak Index in a Mountain Array/ans.py
@@ -0,0 +1,22 @@
+'''
+You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+Return the index of the peak element.
+Your task is to solve it in O(log(n)) time complexity.
+'''
+class Solution(object):
+    def peakIndexInMountainArray(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        start = 1
+        end = len(arr)-2
+        while start <= end:
+            mid = start+ (end- start)/2
+            if arr[mid-1] < arr[mid] > arr[mid+1]:
+                return mid
+            if arr[mid-1] < arr[mid]:
+                start = mid+1
+            else:
+                end = mid-1
+        return -1
