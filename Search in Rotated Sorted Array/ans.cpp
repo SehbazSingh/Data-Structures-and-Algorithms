59f3c5641e7209a1c056f9ab4bd4b1beb5f621c7
Sehbaz Singh <sehbazsingh58@gmail.com>
2025-07-12 18:18:46 +0530

    Update ans.cpp

--- Search in Rotated Sorted Array/ans.cpp
+++ Search in Rotated Sorted Array/ans.cpp
@@ -4,6 +4,7 @@ Prior to being passed to your function, nums is possibly rotated at an unknown p
 Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 */
 
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
